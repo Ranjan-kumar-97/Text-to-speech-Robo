ba203ac8687dfc84165e4e8dd0a1ad9cf8b7b098
Ranjan kumar <kranjan948@gmail.com>
2023-04-21 13:00:16 +0530

    voice choice

--- text_speaker.py
+++ text_speaker.py
@@ -2,10 +2,14 @@ import pyttsx3
 
 
 def text_to_speech():
+    v = input ("Please enter F for Female voice or M for male voice:  ").lower()
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
+    if v == 'm':
+        engine.setProperty("voice", voices[0].id)
+    else:
+        engine.setProperty("voice", voices[1].id)
     name = input("Please enter your name: ")
-    engine.setProperty("voice", voices[0].id)
     welcome_sound = ("Welcome", name , "i am RoboSpeaker version 1 here, to covert you text into speech")
     engine.say(welcome_sound)
     engine.runAndWait()
@@ -13,7 +17,7 @@ def text_to_speech():
     while True:
         word_to_convert = input("Enter what you want me to speak or Q to quit:  ")
         if word_to_convert == "q" or word_to_convert == "Q":
-            bye_sound = ("Pleasure surving you",name, "Bye Bye")
+            bye_sound = ("Pleasure serving you",name, "Bye Bye")
             engine.say(bye_sound)
             engine.runAndWait()
             break
